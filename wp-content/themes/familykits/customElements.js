1519036d36a9807b4ed79549a5b6f7d382cf3bda
KayaKanashiro <KayaKanashiro@users.noreply.github.com>
2025-04-23 00:49:57 +0000

    deploy: d801c49be56948c9ce2b64175790c189121f2e04

---
+++ wp-content/themes/familykits/customElements.js
@@ -0,0 +1,10 @@
+class CopyrightYear extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = new Date().getFullYear();
+    }
+}
+
+customElements.define("x-year", CopyrightYear);
+h1{
+ color: #d18bbe
+}
